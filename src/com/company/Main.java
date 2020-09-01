a0ad94969d45d2b2ec69ec2d2b9f75018e5cb83a
Thomas West <thom8750@stud.kea.dk>
2020-09-01 14:12:55 +0200

    HAN ER MÅSKE EN SVANS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("ROBERT ER EN SVANS");
+	System.out.println("ROBERT ER MÅSKE EN SVANS");
     }
 }
